5642fd10c0a1aa233baf2f3018e7e285b91435a4
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-03-11 13:16:27 +0000

    commit

--- webapp/controller/scroll.controller.js
+++ webapp/controller/scroll.controller.js
@@ -1,12 +1,57 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/viz/ui5/data/FlattenedDataset",
+    "sap/viz/ui5/controls/common/feeds/FeedItem"
+], function (Controller, JSONModel, FlattenedDataset, FeedItem) {
     "use strict";
-
+    var that;
     return Controller.extend("scrollcontainer.controller.scroll", {
-        onInit() {
-            var oModel= this.getOwnerComponent().getModel("sampleJSONModel")
-            this.getView().setModel(oModel)
+        onInit: function () {
+            that=this;
+            var oModel = this.getOwnerComponent().getModel("sampleJSONModel")
+            this.getView().setModel(oModel);
+            this._initializeChart();
+        },
+        _initializeChart: function () {
+            var oVizFrame = this.byId("idVizFrame");
+            var oModel = this.getView().getModel("sampleJSONModel");
+            var oDataset = new FlattenedDataset({
+                dimensions: [{
+                    name: 'Product ID',
+                    value: "{sampleJSONModel>PRODUCT_ID}"
+                }],
+                measures: [{
+                    name: 'Quantity Units',
+                    value: "{sampleJSONModel>QTY_UNITS}"
+                }],
+                data: {
+                    path: "sampleJSONModel>/sampleCollection"
+                }
+            });
+            oVizFrame.setDataset(oDataset);
+            var oFeedCategory = new sap.viz.ui5.controls.common.feeds.FeedItem({
+                'uid': "categoryAxis",
+                'type': "Dimension",
+                'values': ["Product ID"]
+            });
+            var oFeedValue = new sap.viz.ui5.controls.common.feeds.FeedItem({
+                'uid': "valueAxis",
+                'type': "Measure",
+                'values': ["Quantity Units"]
+            });
+            oVizFrame.addFeed(oFeedCategory);
+            oVizFrame.addFeed(oFeedValue);
+            oVizFrame.setVizType("stacked_column");
+        },
+        onUpload: function(){
+            if (!that.upload) 
+                {
+                    that.upload = sap.ui.xmlfragment("scrollcontainer.fragments.upload", that);
+                    that.getView().addDependent(that.upload);
+                }
+                that.upload.open();
         }
     });
 });
+
