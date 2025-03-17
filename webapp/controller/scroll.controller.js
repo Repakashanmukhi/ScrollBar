dc4959a4859884d9b9f2c4e8a516c17e75f46e27
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-03-17 13:34:03 +0000

    changes

--- webapp/controller/scroll.controller.js
+++ webapp/controller/scroll.controller.js
@@ -8,63 +8,72 @@ sap.ui.define([
     var that;
     return Controller.extend("scrollcontainer.controller.scroll", {
         onInit: function () {
-            that=this;
-            var oModel = this.getOwnerComponent().getModel("sampleJSONModel")
-            this.getView().setModel(oModel);
-            this._initializeChart();
+           that=this;
         },
-        _initializeChart: function () {
-            var oVizFrame = this.byId("idVizFrame");
-            var oModel = this.getView().getModel("sampleJSONModel");
-            var oDataset = new FlattenedDataset({
-                dimensions: [{
-                    name: 'Product ID',
-                    value: "{sampleJSONModel>PRODUCT_ID}"
-                }],
-                measures: [{
-                    name: 'Quantity Units',
-                    value: "{sampleJSONModel>QTY_UNITS}"
-                }],
-                data: {
-                    path: "sampleJSONModel>/sampleCollection"
-                }
-            });
-            oVizFrame.setDataset(oDataset);
-            var oFeedCategory = new sap.viz.ui5.controls.common.feeds.FeedItem({
-                'uid': "categoryAxis",
-                'type': "Dimension",
-                'values': ["Product ID"]
-            });
-            var oFeedValue = new sap.viz.ui5.controls.common.feeds.FeedItem({
-                'uid': "valueAxis",
-                'type': "Measure",
-                'values': ["Quantity Units"]
-            });
-            oVizFrame.addFeed(oFeedCategory);
-            oVizFrame.addFeed(oFeedValue);
-            oVizFrame.setVizType("stacked_column");
-        },
-        onDownload: function(oEvent) 
+        onUpload: function()
         {
-            var oTable = this.getView().byId("ProductData");
-            var aItems = oTable.getItems();
-            var aTableData = [
-                ["MANDT", "SALES DOCUMENT", "SALES ITEM", "SCHEDULE LINE NO", "PRODUCT ID", "MATERIAL VARIANT", "UOM", "CONFIRMED QUANTITY", "QUANTITY UNITS", "PRODUCT AVALIABILTY", "NET VALUE", "CUSTOMER GROUP", "LOCATION ID", "SALES ORDER", "DISTR CHANNEL", "DIVISION", "SALES DOCUMENT TYPE"] 
-            ];
-            var oSheet = XLSX.utils.aoa_to_sheet(aTableData);
-            var oWorkbook = XLSX.utils.book_new();
-            XLSX.utils.book_append_sheet(oWorkbook, oSheet, "Employee Data");  
-            var sFileName = "ProductData.xlsx";
-            XLSX.writeFile(oWorkbook, sFileName);
-        }, 
-        onUpload: function(){
             if (!that.upload) 
                 {
                     that.upload = sap.ui.xmlfragment("scrollcontainer.fragments.upload", that);
                     that.getView().addDependent(that.upload);
                 }
                 that.upload.open();
-        }
+        }, 
+        onFileChange: function (oEvent) {
+            var oModel = new JSONModel();
+            var oTable = this.getView().byId("excelData");
+            var excelData = {};
+            var file = oEvent.getParameter("files")[0];
+            oTable.removeAllColumns();
+            oTable.removeAllItems();
+            if (file && window.FileReader) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var data = e.target.result;
+                    var workbook = XLSX.read(data, {
+                        type: 'array'
+                    });
+                    workbook.SheetNames.forEach(function (sheetName) {
+                        excelData = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                    });
+                    oModel.setData({ data: excelData });
+                    oModel.refresh(true);
+                    var inputArr = oModel.getData().data;
+                    var objectKeys = Object.keys(inputArr[0]);
+                    objectKeys.forEach(function (key) {
+                        var oColumn = new sap.m.Column({
+                            header: new sap.m.Label({
+                                text: key
+                            }),
+                        });
+                        oTable.addColumn(oColumn);
+                    });
+                    var tableModel = new JSONModel({ data: inputArr });
+                    oTable.setModel(tableModel);
+                    inputArr.forEach(function (row) {
+                        var cells = [];
+                        for (var i = 0; i < objectKeys.length; i++) {
+                            var key = objectKeys[i];
+                            var cellText = row[key];
+                            cells.push(new sap.m.Text({
+                                text: cellText
+                            }));
+                        }
+                        var oColumnListItem = new sap.m.ColumnListItem({
+                            cells: cells
+                        });
+                        oTable.addItem(oColumnListItem);
+                    });
+                };
+                reader.readAsArrayBuffer(file);
+            }
+            that.close();
+        },
+        close: function(){
+            that.upload.close();
+        } 
     });
 });
+
+
 
