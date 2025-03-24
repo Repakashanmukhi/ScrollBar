f71e0eefdde154fe97cca85fca07d77149995b84
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-03-24 07:04:37 +0000

    commit

--- webapp/controller/scroll.controller.js
+++ webapp/controller/scroll.controller.js
@@ -1,11 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/viz/ui5/data/FlattenedDataset",
-    "sap/viz/ui5/controls/common/feeds/FeedItem",
     "sap/ui/model/odata/ODataModel",  
-    "sap/m/MessageBox"  
-], function (Controller, JSONModel, FlattenedDataset, FeedItem, ODataModel, MessageBox) {
+    "sap/m/MessageBox",
+    "sap/ui/table/Table"
+], function (Controller, JSONModel, ODataModel, MessageBox,Table) {
     "use strict";
     var that;
     return Controller.extend("scrollcontainer.controller.scroll", {
@@ -24,54 +23,64 @@ sap.ui.define([
         },
         onFileChange: function (oEvent) {
             var oModel = new JSONModel();
-            var oTable = this.getView().byId("excelData");
+            var oTable = this.getView().byId("Data");
             var excelData = {};
             var file = oEvent.getParameter("files")[0];
             oTable.removeAllColumns();
-            oTable.removeAllItems();
+            oTable.setModel(); 
             if (file && window.FileReader) {
                 var reader = new FileReader();
                 reader.onload = function (e) {
                     var data = e.target.result;
-                    var workbook = XLSX.read(data, {
-                        type: 'array'
-                    });
+                    var workbook = XLSX.read(data, { type: 'array' });
                     workbook.SheetNames.forEach(function (sheetName) {
                         excelData = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
                     });
                     oModel.setData({ data: excelData });
-                    oModel.refresh(true);
-                    var inputArr = oModel.getData().data;
-                    var objectKeys = Object.keys(inputArr[0]);
+                    oTable.setModel(oModel); 
+                    var array = oModel.getData().data;
+                    var objectKeys = Object.keys(array[0]);
+                    // using sap.ui.table.Column
                     objectKeys.forEach(function (key) {
-                        var oColumn = new sap.m.Column({
-                            header: new sap.m.Label({
-                                text: key
+                        var oColumn = new sap.ui.table.Column({
+                            label: new sap.m.Label({ text: key }),
+                            template: new sap.m.Text({
+                                text: "{" + key + "}" 
                             }),
                         });
                         oTable.addColumn(oColumn);
                     });
-                    var tableModel = new JSONModel({ data: inputArr });
-                    oTable.setModel(tableModel);
-                    inputArr.forEach(function (row) {
-                        var cells = [];
-                        for (var i = 0; i < objectKeys.length; i++) {
-                            var key = objectKeys[i];
-                            var cellText = row[key];
-                            cells.push(new sap.m.Text({
-                                text: cellText
-                            }));
-                        }
-                        var oColumnListItem = new sap.m.ColumnListItem({
-                            cells: cells
-                        });
-                        oTable.addItem(oColumnListItem);
-                    });
+                    oTable.bindRows("/data"); 
                 };
-                reader.readAsArrayBuffer(file);
+                //Using sap.m.Column(m.table)
+                // objectKeys.forEach(function (key) {
+                //     var oColumn = new sap.m.Column({
+                //         header: new sap.m.Label({
+                //             text: key
+                //         }),
+                //     });
+                //     oTable.addColumn(oColumn); 
+                // });
+                // var tableModel = new JSONModel({ data: array });
+                // oTable.setModel(tableModel);
+                // array.forEach(function (row) {
+                //     var cells = [];
+                //     for (var i = 0; i < objectKeys.length; i++) {
+                //         var key = objectKeys[i];
+                //         var cellText = row[key];
+                //         cells.push(new sap.m.Text({
+                //             text: cellText
+                //         }));
+                //     }
+                //     var oColumnListItem = new sap.m.ColumnListItem({
+                //         cells: cells
+                //     });
+                //     oTable.addItem(oColumnListItem);
+                // });
+                reader.readAsArrayBuffer(file);  
             }
-            that.close();
-        },
+            that.close(); 
+        },        
         close: function () {
             that.upload.close();
         },
@@ -91,37 +100,52 @@ sap.ui.define([
             jQuery.ajax({
                 url: sUrl,
                 method: 'GET',
-                dataType: 'json', 
+                dataType: 'json',
                 success: function (data) {
                     oModel.setData(data);
-                    var oTable = that.getView().byId("excelData");
+                    var oTable = that.getView().byId("Data"); 
                     oTable.setModel(oModel);
                     var inputArr = data;
-                        var objectKeys = Object.keys(inputArr[0]);
-                        oTable.removeAllColumns();
-                        oTable.removeAllItems();
-                        objectKeys.forEach(function (key) {
-                            var oColumn = new sap.m.Column({
-                                header: new sap.m.Label({
-                                    text: key
-                                })
-                            });
-                            oTable.addColumn(oColumn);
+                    var objectKeys = Object.keys(inputArr[0]);
+                    oTable.removeAllColumns();
+                    oTable.removeAllRows();
+                    // Using sap.ui.table.Column
+                    objectKeys.forEach(function (key) {
+                        var oColumn = new sap.ui.table.Column({
+                            label: new sap.m.Label({
+                                text: key
+                            }),
+                            template: new sap.m.Text({
+                                text: "{" + key + "}" 
+                            })
                         });
-                        inputArr.forEach(function (row) {
-                            var cells = [];
-                            objectKeys.forEach(function (key) {
-                                var cellText = row[key];  
-                                cells.push(new sap.m.Text({
-                                    text: cellText
-                                }));
-                            });
-                            var oColumnListItem = new sap.m.ColumnListItem({
-                                cells: cells
-                            });
-                            oTable.addItem(oColumnListItem);
-                        });
+                        oTable.addColumn(oColumn);
+                    });
+                    oTable.bindRows("/"); 
                 },
+                // Using sap.m.Column(m.table)
+                // objectKeys.forEach(function (key) 
+                // {
+                //     var oColumn = new sap.m.Column({
+                //         header: new sap.m.Label({
+                //                 text: key
+                //             })
+                //         });     
+                //         oTable.addColumn(oColumn);
+                //     });
+                //     inputArr.forEach(function (row) {
+                //         var cells = [];
+                //         objectKeys.forEach(function (key) {
+                //             var cellText = row[key];  
+                //             cells.push(new sap.m.Text({
+                //                 text: cellText
+                //             }));
+                //         });
+                //         var oColumnListItem = new sap.m.ColumnListItem({
+                //             cells: cells
+                //         });
+                //         oTable.addItem(oColumnListItem);
+                //     });
             });
             that.Close();
         },
